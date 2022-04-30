409a832e5a530962a5c4b15eae97fbc7bdbd9922
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-04-30 15:25:54 +0200

    meta: Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pub_repo",
-    version = "0.1.2",
+    version = "0.1.3",
     url = "https://codeberg.org/PapaTutuWawa/pub_repo",
     author = "Alexander \"PapaTutuWawa\"",
     author_email = "papatutuwawa <at> polynom.me",
